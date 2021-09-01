427f29244291cb29bb8d5e5ffc53d7cabb9f59a9
Prathamesh <prathamesh.bhosale.0201@gmail.com>
2021-09-01 15:16:06 +0530

    Added styles.css

--- f1_analysis/f1_analysis/settings.py
+++ f1_analysis/f1_analysis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,8 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
