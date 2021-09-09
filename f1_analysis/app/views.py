bdda1e705b552de552d8dfb01e345236fa8b459a
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2021-09-09 07:36:37 +0530

    Added modified csv for faster loading

--- f1_analysis/app/views.py
+++ f1_analysis/app/views.py
@@ -10,15 +10,7 @@ def home(request):
     return render(request,'home.html')
 
 def pitstop_analysis(request):
-    pitstop_data = pd.read_csv('Data\pit_stops.csv')
-    pitstop_data = pitstop_data.where(pitstop_data['milliseconds']<50000).dropna()
-    pitstop_data = pitstop_data.groupby(by='raceId').mean()
-    races_data = pd.read_csv(r'Data\races.csv')
-    pitstop_data = pd.merge(races_data,pitstop_data,on='raceId',how='outer')
-    pitstop_data = pitstop_data.groupby(by='circuitId').mean()
-    circuits_data = pd.read_csv('Data\circuits.csv')
-    pitstop_data = pd.merge(circuits_data,pitstop_data,on='circuitId',how='outer').dropna().reset_index()
-    pitstop_data = pitstop_data[['name','milliseconds']]
+    pitstop_data = pd.read_csv('Data\Average_Pitstop.csv')
     fig = px.bar(pitstop_data,x='name',y='milliseconds',width=1280, height = 720,
                  labels=dict(name='Circuit Name',milliseconds='Average Pitstop Time (ms)'))
     fig.update_traces(marker=dict(color= '#646cff'))
